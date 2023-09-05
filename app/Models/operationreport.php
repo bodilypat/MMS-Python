5e32886cbe73a5ed6757ef1703faa66a3c4c192f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-09-05 09:14:34 -0700

    Add files via upload

---
+++ app/Models/operationreport.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Elquent\SoftDeletes;
+
+class operationreport extends Model
+{
+
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+            'patient',
+            'description',
+            'doctor',
+            'time'      
+    ];
+
+}
