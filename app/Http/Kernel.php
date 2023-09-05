0228218e33d26047fd4594db4f5f435c6f6987af
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-09-05 09:55:03 -0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,7 +1,45 @@
 <?php
-namespace App\Http;
-use Illuminate\Foundation\Http\Kernel as HttpKernal;
 
-Class Kernel extends HttpKernel
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernal as HttpKernal;
+
+class Kernal extends HttpKernel
 {
-  }
+    protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimString::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyToNull::class,
+    ];
+
+    protected $middlewareGroup = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api'=> [
+            \Illuminate\Routing\Middleware\SubStituteBindings::class,
+        ],
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'checksuperadmin' => \App\Http\Middleware\checksupperadmin::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetChacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'quest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' =>\Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+}
