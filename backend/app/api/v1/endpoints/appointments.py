5d5b9fb200d1e51559d4e8db78ce62eb8e9d1311
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-05 15:16:23 -0700

    Add files via upload

--- backend/app/api/v1/endpoints/appointments.py
+++ backend/app/api/v1/endpoints/appointments.py
@@ -1,10 +1,46 @@
 # backend/app/api/v1/endpoints/appointments.py
 
-from fastapi import APIRouter
-from app.schemas import AppointmentCreate 
-from app.services.appointment import create_appointment
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-router = APIRouter("/")
+
+from app.schemas import AppointmentCreate, AppointmentOut
+from app.services.appointment import create_appointment, list_appointment, get_appointment_by_id
+from app.api.v1 import deps 
+from typing import List, Optional
+
+router = APIRouter(prefix="/appointments", tags=["Appointments"])
+
+@router.post("/", response_model=AppointmentOut, status_code=201)
+    def book_appointment(
+        data: AppointmentCreate,
+        db: Session = Depends(deps.get_db)
+     ):
+     return create_appointment(db=db, appointment=data)
+
+@router.get("/", response_model=List[AppointmentOut])
+    def get_all_appointments(
+            db: Session = Depends(deps.get_db),
+            doctor_id: Optional[str] = None, 
+            patient_id: Optional[str] = None, 
+            skip: int = 0,
+            limit: int = 100 
+       ):
+        return list_appointments(
+            db=db,
+            skip=limit,
+            limit=limit,
+            doctor_id=doctor_id 
+            patient_id=patient_id
+       )
+       
+@router.get("/{appointment_id}", response_model=AppointmentOut)
+    def get_appointment(appointment_id: str, db: Session = Depends(deps.get_db)):
+    if not appoointment:
+        raise HTTPException(status_code=404, detail="Appointment not found")
+    return appointment
+        
+    
 def book_appointment(data: AppointmentCreate):
 	return create_appointment(data)
 	
