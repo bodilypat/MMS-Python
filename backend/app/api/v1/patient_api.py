d23be788458823c3ace206b5fc90783401fed8b2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-03 08:57:01 -0700

    Add files via upload

---
+++ backend/app/api/v1/patient_api.py
@@ -0,0 +1,48 @@
+#app/api/v1/patient_api.py
+
+from fastapi import APIRouter, Depends, HTTPException, status 
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.schemas import patient_schema
+from app.services import patient_service
+from app.db import get_db 
+
+router = APIRouter(prefix="/patients", tags=["Patients"])
+
+@router.post("/", response_model=patient_schema.PatientOut)
+def create_patient(patient:patient_schema.PatientCreate, db: Session = Depends(get_db)):
+    new_patient = models.Patient(**patient.dict())
+    db.add(new_patient)
+    db.commit()
+    db.refresh(new_patient)
+    return new_patient
+    
+# Gall all patient 
+@router.get("/", response_model=List[schemas.PatientOut])
+def get_all_patients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return services.get_all_patients(db, skip, limit)
+    
+# Get patient by ID
+@router.get("/{patient_id}", response_model=patient_schema.PatientOut)
+def get_patient_by_id(patient_id: int, db: Session = Depends(get_db)):
+    patient = services.get_patient_by_id(db, patient_id)
+    if not patient:
+        raise HTTPException(status_code = 404, detail="Patient not found")
+    return Patient
+    
+# Update patient 
+@router.put("/{patient_id}", response_model=patient_schema.PatientOut)
+def update_patient(patient_id: int, patient: schemas.PatientUpdate, db: Session = Depends(get_db)):
+    updated = services.update_patient(db, patient_id, patient)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    return updated
+  
+# Delete patient 
+@router.delete("/{patient_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_patient(patient_id: int, db: Session = Depends(get_db)):
+    deleted = services.delete_patient(db, patient_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail ="Patient not found")
+        
