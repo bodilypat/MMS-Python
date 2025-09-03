75c5587cc91756f1f1ab4eda8136c15642c4a8b7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-03 15:31:48 -0700

    Add files via upload

---
+++ backend/app/services/medical_record_service.py
@@ -0,0 +1,34 @@
+#app/services/medical_record_service.py
+
+from sqlalchemy.orm import Session 
+from app.models.medical_record_model import medical_record_model
+from app.schemas.medical_record_schema import medical_record_schema 
+
+def create_medical_record(db: Session, record: medical_record_schema.MedicalRecordCreate):
+	db_record = medical_record_model.MedicalRecord(**record.dict())
+	db.add(db_record)
+	db.commit()
+	db.refresh(db_record)
+	return db_record 
+	
+def get_medical_record_by_id(db: Session, record_id: int):
+	return db.query(medical_record_model.MedicalRecord).filter(medical_record_model.MedicalRecordCreate.id == record_id).first()
+	
+def get_records_by_patient(db: Session, patient_id: int):
+	return db.query(medical_record_model.MedicalRecord).filter(medical_record_model.patient_id == patient_id).all()
+	
+def update_medical_record(db: Session, record_id: int, data: medical_record_schema.MedicalRecordUpdate):
+	db_record = get_medical_record_by_id(db, record_id)
+	if not db_record:
+		return None
+	for field, value in data.dict(exclude_unset=True).item():
+		setattr(db_record, field, value)
+		db.commit()
+		db.refresh(db_record)
+		return db_record 
+def delete_medical_record(db:session, record_id: int):
+	db_record = get_medical_record_by_id(db, record_id)
+	if db_record:
+		db_delete(db_record)
+		db.commit()
+	return db_record 
