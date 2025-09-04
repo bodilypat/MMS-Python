b0054b1dd2fc5c2500b871a21fc3ea09d9813a74
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-04 09:38:55 -0700

    Add files via upload

--- backend/app/services/patient_service.py
+++ backend/app/services/patient_service.py
@@ -4,37 +4,42 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, status 
 from typing import List, Optional
 
-from app import model, schemas 
+from app.models import patient_model as models
+from app.schemas import patient_schema as schemas
 
-def create_patient(db: Session, patient: schemas.PatientCreate):
+def create_patient(db: Session, patient: schemas.PatientCreate) -> models.Patient:
 	db_patient = models.Patient(**patient.dict())
 	db.add(db_patient)
 	db.commit()
 	db.refresh(db_patient)
 	return db_patient
 	
-def get_patient_by_id(db: Session, patient_id: int):
+def get_patient_by_id(db: Session, patient_id: int) -> Optional[models.Patient]:
 	return db.query(models.Patient).filter(models.Patient.id == patient_id).first()
 	
-def get_all_patients(db: Session, skip: int =0, limit = 100):
+def get_all_patients(db: Session, skip: int = 0, limit = 100) ->List[models.Patient]:
 	return db.query(models.Patient).offset(skip).limit(limit).all()
 	
-def update_patient(db: Session, patient_id: int, patient: schemas.PatientUpdate):
+def update_patient(db: Session, patient_id: int, patient: schemas.PatientUpdate) ->Optional[models.Patient]:
 	db_patient = get_patient_by_id(db, patient_id)
 	if not db_patient:
 		return None 
-		for field, value in patient.dict(exclude_unset=True).items():
+        
+    patient_data = patient.dict(exclude_unset=True)
+	for field, value in patient_data.items():
 		setattr(db_patient, field, value)
 		
 		db.commit()
 		db.refresh(db_patient)
 		return db_patient 
 		
-def delete_patient(db: Session, patient_id: int):
-	db_patient = get_patient_by_id(db, Patient)
+def delete_patient(db: Session, patient_id: int) -> bool:
+	db_patient = get_patient_by_id(db, patient_id)
 	if not db_patient:
-		db.delete(db_patient)
-		db.commit()
-	return db_patient 
+        return False
+        
+	db.delete(db_patient)
+	db.commit()
+	return True 
 	
 	
