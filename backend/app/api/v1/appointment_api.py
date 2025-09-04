6e1f8dd36bf9522bca4e7fe4fcbad4d20134788c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-04 14:15:03 -0700

    Add files via upload

--- backend/app/api/v1/appointment_api.py
+++ backend/app/api/v1/appointment_api.py
@@ -2,41 +2,49 @@
 
 from fastapi import APIRouter, Depends, HTTPException,
 from sqlalchemy.orm import Session
-from app.services.appointment_service import appointment_service 
-from app.schemas.appointment_schema import appointment_schema 
+
+from app.services import appointment_service 
+from app.schemas import appointment_schema 
+from app.db.session import get_db
 
 router = APIRouter(prefix="/appointments", tags=["Appointments"])
 
-@router.post("/", response_model=appointment_schema.AppointmentOut)
-def create(appointment: appointment_schema.AppointmentCreat, db: Session = Depends(get_db)):
+# Create a new appointment
+@router.post("/", response_model=appointment_schema.AppointmentOut, status_code=status.HTTP_201_CREATED)
+def create_appointment(appointment: appointment_schema.AppointmentCreat, db: Session = Depends(get_db)):
 	try:
 		return appointment_service.create_appointment(db, appointment)
 	except ValueError as e:
-		raise HTTPException(status_code=400, detail=str(e))
-		
+		raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+# Get an appointment by Id		
 @router.get("/{appointment_id}", response_model=appointment_schema.AppointmentOut)
-def read(appointment_id: int, db: Session = Depends(get_db)):
+def get_appointment(appointment_id: int, db: Session = Depends(get_db)):
 	appointment = appointment_service.get_appointment_by_id(db, appointment_id)
 	if not appointment:
-		raise HTTPException(status_code=404, detail="Appointment not found")
+		raise HTTPException(status_code=status.HTTP_404_FOUND, detail="Appointment not found")
 	return appointment 
 	
+# list all appointment
 @router.get("/", response_model=list[appointment_schema.AppointmentOut])
-def list(skip: int =0, limit: int = 100, db: Session = Depends(get_db)):
+def list_appointment(skip: int =0, limit: int = 100, db: Session = Depends(get_db)):
 	return appointment_service.get_all_appointments(db, skip, limit)
 	
-@router.put("/{appointment_di}", response_model=appointment_schema.AppointmentOut)
-def update(appointment_id: int, data: appointment_schema.AppointmentUpdate, db: Session = Depends(get_db)):
+# Update appointment by ID
+@router.put("/{appointment_id}", response_model=appointment_schema.AppointmentOut)
+def update_appointment(appointment_id: int, data: appointment_schema.AppointmentUpdate, db: Session = Depends(get_db)):
 	updated = appointment_service.update_appointment(db, appointment_id, data)
 	if not updated:
-		raise HTTPException(status_code=404, detail="Appointment not found")
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Appointment not found")
 	return updated 
 	
-@router.delete("/{appointment_id}")
-def delete(appointment_id: int, db: Session = Depends(get_db)):
+# Delete appointment by ID
+@router.delete("/{appointment_id}", status_code=status.HTTP_201_NO_CONTENT)
+def delete_appointment(appointment_id: int, db: Session = Depends(get_db)):
 	deleted = appointment_service.delete_appointment(db, appointment_id)
 	if not deleted:
-		raise HTTPException(status_code=404, detail="Appointment not found")
-	return {"message": "Deleted successfully"}
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Appointment not found")
+	return None
+    
 	
 	
