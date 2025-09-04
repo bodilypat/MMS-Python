821a741f6280e54f89c9e445f5a657289d40b595
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-04 12:08:12 -0700

    Add files via upload

--- backend/app/services/doctor_service.py
+++ backend/app/services/doctor_service.py
@@ -1,37 +1,41 @@
 #app/services/doctor_service.py 
 
 from sqlalchemy.orm import Session
-from app.models.doctor_model import doctor_model
-from app.schemas.doctor_schema import doctor_schema
+from app.models import doctor_model
+from app.schemas import doctor_schema
 
-def create_doctor(db: Session, doctor: doctor_schema.DoctorCreate):
-	db_doctor = doctor_model.Doctor(**doctor.doct())
-	db.add(doctor)
+def create_doctor(db: Session, doctor: doctor_schema.DoctorCreate) -> doctor_model.Doctor:
+	db_doctor = doctor_model.Doctor(**doctor.dict())
+	db.add(db_doctor)
 	db.commit()
 	db.refresh(db_doctor)
 	return db_doctor 
 	
-def get_doctor_by_id(db: Session, doctor_id: int):
+def get_doctor_by_id(db: Session, doctor_id: int) -> doctor_model.Doctor | None:
 	return db.query(doctor_model.Doctor).filter(doctor_model.Doctor.id == doctor_id).first()
 	
-def get_all_doctors(db: Session, skip: int = 0, limit: int = 100):
+def get_all_doctors(db: Session, skip: int = 0, limit: int = 100) -> list[doctor_model.Doctor]:
 	return db.query(doctor_model.Doctor).offset(skip).limit(limit).all()
 
-def update_doctor(db: Session, doctor_id: int, doctor:doctor_schema.DoctorUpdate):
+def update_doctor(db: Session, doctor_id: int, doctor:doctor_schema.DoctorUpdate) -> doctor_model.Doctor | None:
 	db_doctor = get_doctor_by_id(db, doctor_id)
 	if not db_doctor:
 		return None 
+        
 	for field, value in doctor.dict(exclude_unset=True).items():
 		setattr(db_doctor, field, value)
+        
 	db.commit()
 	db.refresh(db_doctor)
 	return db_doctor 
 
-def delete_doctor(db:session, doct_id: int)
+def delete_doctor(db:session, doctor_id: int) -> bool:
 	db_doctor = get_doctor_by_id(db, doctor_id)
-	if db_doctor:
+	if not db_doctor:
+        return False
+        
 		db.delete(db_doctor)
 		db.commit()
-	return db_doctor 
+	return True 
 	
 	
