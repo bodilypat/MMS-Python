ae7c967dc80b20862e6d5fc35c5d748674d936e7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-04 22:17:32 -0700

    Add files via upload

--- backend/app/schemas/billing_schema.py
+++ backend/app/schemas/billing_schema.py
@@ -1,10 +1,11 @@
 #app/schemas/billing_schema.py
 
-from pydanti import BaseModel 
+from pydanti import BaseModel, Field 
 from typing import List, Optional
 from enum import Enum
 from datetime import datetime
 
+# Enums
 class PaymentStatus(str, Enum):
 	pending = "pending"
 	paid = "paid"
@@ -16,9 +17,11 @@ class PaymentMethods(str, Enum):
 	insurance = "insurance"
 	upi = "upi"
 	
+# Bill Item Schemas 
 class BillItemBase(BaseModel):
 	description: str 
-	cost: float
+	cost: float = Field(..., ge=0)
+    quantity: int = Field(default=1, ge=1)
 	
 class BillItemCreate(BillItemBase):
 	pass 
@@ -29,8 +32,40 @@ class BillItemOut(BillItemBase):
 	class Config:
 		orm_mode = True 
 	
+# Main Bill Schemas
 class BillBase(BaseModel):
-	items: List[BillItemCreate]
+	patient_id: int 
+    appoitment_id: Optional[int] = None 
+    method: Optional[str] = None 
+    notes: Optional[str] = None 
+    status: Optional[PaymentStatus] = PaymentStatus.pending
+    created_by: Optional[int] = None
+    updated_by: Optional[int] = None 
+    items: List[BillItemCreate]
+    
+    class BillCreate(BillBase):
+        pass 
+        
+class BillOut(BaseModel):
+    bill_id: int 
+    patient_id: int 
+    appointment_id: Optional[int]
+    method: PaymentMethods
+    notes: Optional[str]
+    status: PaymentStatus 
+    amount: float
+    issued_at: datetime 
+    created_by: Optional[int]
+    updated_by: Optional[int]
+    items: List[BillItemOut]
+    
+    class Config:
+        orm_mode =  True 
+        
+# For updating status only
+class BillUpdateStatus(BaseModel):
+    status: PaymentStatus 
+    
 	
 class BillOut(BaseBase):
 	id: int 
