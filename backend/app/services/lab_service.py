2673c7f8dafb757997118b83d30ac6d0d33df29d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-05 09:25:33 -0700

    Add files via upload

--- backend/app/services/lab_service.py
+++ backend/app/services/lab_service.py
@@ -1,40 +1,51 @@
 #app/services/lab_service.py
 
 from sqlalchemy.orm import Session
-from app.models.lab_model import lab_model
-from app.schemas.lab_schema import lab_schema 
+from app.models import lab_model
+from app.schemas import lab_schema 
 from datetime import datetime 
+from typing import List, Optional
 
-def create_lab_test(db: Session, test: lab_schema.LabTestCreate):
+# Lab Test Service
+def create_lab_test(db: Session, test: lab_schema.LabTestCreate) -> lab_model.LabTest:
 	db_test = lab_model.LabTest(**test.dict())
 	db.add(db_test)
 	db.commit()
 	db.refresh(db_test) 
 	return db_test
 
-def list_bal_tests(db: Session):
+def list_bal_tests(db: Session)-> List[lab_model.labTest]:
 	return db.query(lab_model.LabTest).all()
-	
-def create_lab_order(db: Session, order: lab_schema.LabTestCreate):
+
+# Lab Order Services	
+def create_lab_order(db: Session, order: lab_schema.LabTestCreate) -> lab_model.LabOrder:
 	db_order = lab_model.LabOrder(**order.dict())
 	db.add(db_order)
 	db.commit()
 	db.refresh(db_order)
 	return db_order 
 	
-def update_lab_order(db: Session, order_id: int, data: lab_schema.LabOrderUpdate):
-	order = db.query(lab_model.LabOrder).filter(lab_model.LabOrder.id == order_id).first()
-	if not order:
-		return None 
-	for field, value in data.dict(exclude_unset=True).items():
-		setattr(order, field, value)
-	if data.status == lab_schema.TestStatus.completed:
-		order.completed_at = datetime.utcnow()
-		
-	db.commit()
-	db.refresh(order)
-	return order 
-	
-def get_orders_by_patient(db: Session, patient_id: int):
-	return db.query(lab_model.LabOrder).filer_by(patient_id=patient_id).all()
+def get_lab_order_by_id(db: Session, order_id: int) -> Optional[lab_model.LabOrder]:
+    return db.query(lab_model.LabOrder).filter(lab_model.LabOrder.id == order_id).first()
+    
+def update_lab_order(db: Session, order_id: int, data: lab_schema.LabOrderUpdate) -> Optional[lab_model.LabOrder]:
+    order = db.query(lab_model.LabOrder).filter(lab_model.LabOrder.id == order_id).first()
+    
+    if not order:
+        return None 
+        
+        update_data = data.dict(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(order, field, value)
+            
+        if "status" in update_data and update_data["status"] == lab_schema.TestStatus.completed:
+            order.completed_at = datetime.utcnow()
+            
+        db.commit()
+        db.refresh(order)
+        return order 
+        
+def get_order_by_patient(db: Session, patient_id: int) -> List[lab_model.LabOrder];
+    return db.query(lab_model.LabOrder).filter_by(patient_id=patient_id).all()
+    
 	
