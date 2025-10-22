1c31cc66539b4a89f1c9af2c74ac1f6301a5bfb4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-10-22 12:43:43 -0700

    Add files via upload

--- backend/app/api_router.py
+++ backend/app/api_router.py
@@ -1,29 +1,31 @@
 # app/api_router.py
 
 from fastapi import APIRouter 
-from app.api.v1 import(
-		patient_api,
-		doctor_api,
-		appointment_api,
-		prescription_api,
-		medical_record_api,
-		billing_api,
-        inventory_api,
-        lab_api,
+from app.api.endpoints import(
+		patient_router,
+        department_router,
+		doctor_router,
+		appointment_router,
+		prescription_router,
+		medical_record_router,
+		billing_router,
+        inventory_router,
+        lab_router,
 	)
     
-	# Create the main API Router
-	api_router = APIRouter(prefix="/api/v1")
+# Create the main API Router
+api_router = APIRouter(prefix="/api")
     
-    # Include individual routers
-	api_router.include_router(patient_api.router, prefix="/patients", tags=["Patients"])
-    api_router.include_router(doctor_api.router, prefix="/doctors", tags=["Doctors"])
-    api_router.include_router(appointment_api.router, prefix="/appointments", tags=["Appointments"])
-    api_router.include_router(prescription_api.router, prefix="/prescriptions", tags=["Prescriptions"])
-    api_router.include_router(medical_record_api.router, prefix="/medical-record", tags=["Medical records"])
-    api_router.include_router(billing_api.router, prefix="/billings", tags=["Biling"])
-    api_router.include_router(inventory_api.router, prefix="inventory", tags=["Inventory"])
-    api.router.include_router(lab_api.router, prefix="/lab", tags="Lab Management"])
+# Include individual routers
+api_router.include_router(patient_router.router, prefix="/patients", tags=["Patients"])
+api_router.include_router(department_router.router, prefix="/departments", tags=["Departments"])
+api_router.include_router(doctor_router.router, prefix="/doctors", tags=["Doctors"])
+api_router.include_router(appointment_router.router, prefix="/appointments", tags=["Appointments"])
+api_router.include_router(prescription_router.router, prefix="/prescriptions", tags=["Prescriptions"])
+api_router.include_router(medical_record_router.router, prefix="/records", tags=["Medical records"])
+api_router.include_router(billing_router.router, prefix="/billings", tags=["Billing"])
+api_router.include_router(inventory_router.router, prefix="inventory", tags=["Inventory"])
+api_router.include_router(lab_api.router, prefix="/labs", tags=["Labs"])
     
     
 	
