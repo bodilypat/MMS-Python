ddd0f08f59e88ebe450b14c7255f7c78b526aef2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-10-23 18:04:46 -0700

    Create Navbar.jsx

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,14 @@
+/* < Navbar.jsx  */
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2>Medical Management</h2>
+            <div className="navbar-actions">
+                <span>User</span>
+            </div>
+        </nav>
+    );
+};
