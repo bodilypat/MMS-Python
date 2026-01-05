12b823daffa4a8ba6d2d9589218130e5e6dd53ba
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-01-05 09:53:07 -0800

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,67 @@
-#app/main.py
+# app/main.py 
+
+""" 
+    FastAPI application entry point for Medical Management System(MMS)
+    Responsibilities:
+    - Load environment configuration
+    - Initialize logging
+    - Connect database 
+    - Register middleware
+    - Include API routers
+"""
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from app.routers import (
-    auth,
-    users,
-    patients,
-    appointnents,
-    doctors,
-    medical_records,
-    prescriptions,  
-    billing,
-)
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI("medical Management System")
-app.include_router(auth.router)
-app.include_router(users.router)
-app.include_router(patients.router)
-app.include_router(appointnents.router)
-app.include_router(doctors.router)
-app.include_router(medical_records.router)
-app.include_router(prescriptions.router)
-app.include_router(billing.router)
+from app.config import settings
+from app.core.logging import init_logging
+from app.db.database import engine 
+from app.models.base import Base
+from app.api.v1.api_router import api_router
 
+#--------------------------------------------
+# Application Lifespan Context Manager(Start & Shoutdown )
+#--------------------------------------------
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup Code
+    init_logging()
+    Base.metadata.create_all(bind=engine)
+
+    app.logger.info(" MMS Backend started successfully")
+
+    yield
+    
+    # Shutdown Code
+    app.logger.info(" MMS Backend shutdown completed")
+
+#--------------------------------------------
+# Create FastAPI Application Instance
+#--------------------------------------------
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Medical Management System (MMS) Backend",
+        description="Backend API for Medical Management System",
+        version="1.0.0",
+        docs_url="/docs",
+        redoc_url="/redoc",
+        lifespan=lifespan
+    )
+
+    # Register Middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ALLOW_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # Include API Routers
+    app.include_router(api_router, prefix="/api/v1")
+    return app
+app = create_app()
+#--------------------------------------------
+# End of main.py
+#--------------------------------------------
